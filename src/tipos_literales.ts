757717d758ff8d878bb9d716f409c0460dcac0fe
Emiliojoa <emiliojoaquinortizmalich@gmail.com>
2025-08-21 14:33:10 -0300

    realice las 12

---
+++ src/tipos_literales.ts
@@ -0,0 +1,16 @@
+
+let direction: "up" | "down" | "left" | "right";
+
+direction = "up";
+console.log(`Dirección actual: ${direction}`);
+
+direction = "right";
+console.log(`Nueva dirección: ${direction}`);
+
+direction = "down"
+console.log(`Nueva dirección: ${direction}`);
+
+direction = "left"
+console.log(`Nueva dirección: ${direction}`);
+
+
