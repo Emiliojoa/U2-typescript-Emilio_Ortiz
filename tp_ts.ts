b364573656972636fdede146c51e8fdb1698b153
Emiliojoa <emiliojoaquinortizmalich@gmail.com>
2025-08-21 11:41:07 -0300

    realice las 2 primeras actividades

---
+++ tp_ts.ts
@@ -0,0 +1,3 @@
+const age :number = 18
+const nombre: string = "hola, empece typeScript"
+//utilizo la variable nombre porque "name" es una palabra reservada de typeScript
---
+++ union_types.ts
@@ -0,0 +1,10 @@
+let estado: string | number = "online"
+console.log("Status como string:", estado)
+
+
+estado = 200
+console.log("Status como número:", estado)
+
+console.log(estado)
+
+// uso la palbara estado y no "status" porque es otra palabra resserada de ts
